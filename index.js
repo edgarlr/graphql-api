37fceb6c1530a5a75f9b96ec31b145b9f3e807ab
Edgar López <edgar.lr.18@gmail.com>
2020-09-08 18:40:12 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const { config } = require('./config')
+const { makeExecutableSchema } = require('graphql-tools')
+const express = require('express')
+const cors = require('cors')
+const { graphqlHTTP } = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+
+const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 
+'utf-8'
+)
+
+const schema = makeExecutableSchema({typeDefs, resolvers})
+
+app.use(cors())
+
+app.use('/api', graphqlHTTP({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: config.dev
+}))
+
+app.listen(config.port, () => {
+  console.log(`Server is listening at http://localhost:${config.port}/api`);
+})
